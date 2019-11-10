80c8362400af6e38b67789906e30bfe136773ea6
ShresthaGaurav <gauravshrestha202@gmail.com>
2019-11-10 23:05:24 +0545

    Validation

--- app/src/main/java/com/shresthagaurav/tablayouthw/NavMain.java
+++ app/src/main/java/com/shresthagaurav/tablayouthw/NavMain.java
@@ -59,4 +59,5 @@ public class NavMain extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
